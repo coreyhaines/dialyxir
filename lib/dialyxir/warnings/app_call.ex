4d0c4924709da1c8d40de7497971c465f4785e60
Jeremy Huffman <jeremy@jeremyhuffman.com>
2018-07-18 07:59:11 -0400

    Revise app_call.

--- lib/dialyxir/warnings/app_call.ex
+++ lib/dialyxir/warnings/app_call.ex
@@ -7,9 +7,10 @@ defmodule Dialyxir.Warnings.AppCall do
 
   @impl Dialyxir.Warning
   @spec format_short([String.t()]) :: String.t()
-  def format_short([module, function, arity, _culprit, _expected_type, _actual_type]) do
+  def format_short([module, function, _arity, culprit, _expected_type, _actual_type]) do
     pretty_module = Dialyxir.PrettyPrint.pretty_print(module)
-    "Module or function to apply is not an atom: #{pretty_module}.#{function}/#{arity}."
+    pretty_culprit = Dialyxir.PrettyPrint.pretty_print(culprit)
+    "#{pretty_culprit} in #{pretty_module}.#{function} is not an atom."
   end
 
   @impl Dialyxir.Warning
